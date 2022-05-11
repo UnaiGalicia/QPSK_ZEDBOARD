d0619c08cbc0df9c919c5197efef733676480497
Unai <unaigalicia99@gmail.com>
2022-05-11 18:17:49 +0200

    Adding the IP of the Hamster that duplicates 24 of the 32 bits of the AXI

---
+++ HamsterIP/src/design_1.vhd
@@ -0,0 +1,9 @@
+--Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+--Date        : Wed Apr 27 11:55:02 2022
+--Host        : AINTZEL-PC running 64-bit major release  (build 9200)
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
