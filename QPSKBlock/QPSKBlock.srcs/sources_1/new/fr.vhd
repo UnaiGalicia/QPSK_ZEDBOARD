c4beb5821f34ec430c0e2089b8559b1a9934898b
Unai <unaigalicia99@gmail.com>
2022-05-13 18:21:10 +0200

    Adding empty QPSKBlock

---
+++ QPSKBlock/QPSKBlock.srcs/sources_1/new/fr.vhd
@@ -0,0 +1,61 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 11.05.2022 09:12:58
+-- Design Name: 
+-- Module Name: fr - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity fr is
+--    Port ( audio_serie : in STD_LOGIC_VECTOR (31 downto 0);
+--           audio_L : out STD_LOGIC_VECTOR (23 downto 0) := (others => '0');
+--           audio_R : out STD_LOGIC_VECTOR (23 downto 0) := (others => '0');
+--           nsample: in STD_LOGIC;
+--           axi_tvalid: in STD_LOGIC;
+--           axi_tready: out STD_LOGIC;
+--           clk : in STD_LOGIC);
+--end qpsk_egokitu;
+
+end fr;
+
+architecture Behavioral of fr is
+
+--signal
+
+begin
+
+--process(clk)
+    variable count16 : natural range 0 to 15 := 0 ;
+--   begin
+--      if rising_edge(clk) then
+      
+--      end if;
+--end process;
+
+
+end Behavioral;
