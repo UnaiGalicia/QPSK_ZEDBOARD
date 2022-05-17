e42cd88382ff7979ecea709e00675180ad43f703
Unai <unaigalicia99@gmail.com>
2022-05-17 16:47:04 +0200

    QPSK Block updated, AXI Stream modifying left

---
+++ QPSKBlock/QPSKBlock2.sim/sim_1/behav/xsim/xsim.dir/QPSKTB_behav/obj/xsim_1.c
@@ -0,0 +1,214 @@
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+/**********************************************************************/
+/*   ____  ____                                                       */
+/*  /   /\/   /                                                       */
+/* /___/  \  /                                                        */
+/* \   \   \/                                                         */
+/*  \   \        Copyright (c) 2003-2013 Xilinx, Inc.                 */
+/*  /   /        All Right Reserved.                                  */
+/* /---/   /\                                                         */
+/* \   \  /  \                                                        */
+/*  \___\/\___\                                                       */
+/**********************************************************************/
+
+
+#include "iki.h"
+#include <string.h>
+#include <math.h>
+#ifdef __GNUC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#define alloca _alloca
+#endif
+typedef void (*funcp)(char *, char *);
+extern int main(int, char**);
+extern void execute_2(char*, char *);
+extern void execute_3(char*, char *);
+extern void execute_4(char*, char *);
+extern void execute_5(char*, char *);
+extern void execute_6(char*, char *);
+extern void execute_7(char*, char *);
+extern void execute_8(char*, char *);
+extern void execute_9(char*, char *);
+extern void execute_66(char*, char *);
+extern void execute_67(char*, char *);
+extern void execute_34(char*, char *);
+extern void vlog_const_rhs_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_196(char*, char *);
+extern void execute_202(char*, char *);
+extern void execute_203(char*, char *);
+extern void execute_212(char*, char *);
+extern void execute_213(char*, char *);
+extern void execute_214(char*, char *);
+extern void execute_215(char*, char *);
+extern void execute_216(char*, char *);
+extern void execute_218(char*, char *);
+extern void execute_223(char*, char *);
+extern void execute_224(char*, char *);
+extern void execute_225(char*, char *);
+extern void execute_226(char*, char *);
+extern void execute_227(char*, char *);
+extern void execute_37(char*, char *);
+extern void execute_65(char*, char *);
+extern void vlog_simple_process_execute_0_fast_no_reg_no_agg(char*, char*, char*);
+extern void execute_105(char*, char *);
+extern void execute_186(char*, char *);
+extern void execute_187(char*, char *);
+extern void execute_188(char*, char *);
+extern void execute_189(char*, char *);
+extern void execute_190(char*, char *);
+extern void execute_191(char*, char *);
+extern void execute_192(char*, char *);
+extern void execute_46(char*, char *);
+extern void execute_47(char*, char *);
+extern void execute_48(char*, char *);
+extern void execute_62(char*, char *);
+extern void execute_63(char*, char *);
+extern void execute_64(char*, char *);
+extern void execute_118(char*, char *);
+extern void execute_119(char*, char *);
+extern void execute_120(char*, char *);
+extern void execute_121(char*, char *);
+extern void execute_122(char*, char *);
+extern void execute_123(char*, char *);
+extern void execute_124(char*, char *);
+extern void execute_126(char*, char *);
+extern void execute_127(char*, char *);
+extern void execute_128(char*, char *);
+extern void execute_129(char*, char *);
+extern void execute_133(char*, char *);
+extern void execute_137(char*, char *);
+extern void execute_138(char*, char *);
+extern void execute_139(char*, char *);
+extern void execute_140(char*, char *);
+extern void execute_141(char*, char *);
+extern void execute_142(char*, char *);
+extern void execute_145(char*, char *);
+extern void execute_147(char*, char *);
+extern void execute_148(char*, char *);
+extern void execute_149(char*, char *);
+extern void execute_150(char*, char *);
+extern void execute_151(char*, char *);
+extern void execute_152(char*, char *);
+extern void execute_153(char*, char *);
+extern void execute_154(char*, char *);
+extern void execute_155(char*, char *);
+extern void execute_156(char*, char *);
+extern void execute_157(char*, char *);
+extern void execute_158(char*, char *);
+extern void execute_159(char*, char *);
+extern void execute_160(char*, char *);
+extern void execute_50(char*, char *);
+extern void execute_51(char*, char *);
+extern void execute_52(char*, char *);
+extern void execute_53(char*, char *);
+extern void execute_130(char*, char *);
+extern void execute_131(char*, char *);
+extern void execute_132(char*, char *);
+extern void execute_55(char*, char *);
+extern void execute_56(char*, char *);
+extern void execute_57(char*, char *);
+extern void execute_58(char*, char *);
+extern void execute_134(char*, char *);
+extern void execute_135(char*, char *);
+extern void execute_136(char*, char *);
+extern void execute_60(char*, char *);
+extern void execute_61(char*, char *);
+extern void vlog_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void transaction_32(char*, char*, unsigned, unsigned, unsigned);
+extern void vhdl_transfunc_eventcallback(char*, char*, unsigned, unsigned, unsigned, char *);
+extern void transaction_38(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_39(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_40(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_49(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_50(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_51(char*, char*, unsigned, unsigned, unsigned);
+extern void transaction_52(char*, char*, unsigned, unsigned, unsigned);
+funcp funcTab[102] = {(funcp)execute_2, (funcp)execute_3, (funcp)execute_4, (funcp)execute_5, (funcp)execute_6, (funcp)execute_7, (funcp)execute_8, (funcp)execute_9, (funcp)execute_66, (funcp)execute_67, (funcp)execute_34, (funcp)vlog_const_rhs_process_execute_0_fast_no_reg_no_agg, (funcp)execute_196, (funcp)execute_202, (funcp)execute_203, (funcp)execute_212, (funcp)execute_213, (funcp)execute_214, (funcp)execute_215, (funcp)execute_216, (funcp)execute_218, (funcp)execute_223, (funcp)execute_224, (funcp)execute_225, (funcp)execute_226, (funcp)execute_227, (funcp)execute_37, (funcp)execute_65, (funcp)vlog_simple_process_execute_0_fast_no_reg_no_agg, (funcp)execute_105, (funcp)execute_186, (funcp)execute_187, (funcp)execute_188, (funcp)execute_189, (funcp)execute_190, (funcp)execute_191, (funcp)execute_192, (funcp)execute_46, (funcp)execute_47, (funcp)execute_48, (funcp)execute_62, (funcp)execute_63, (funcp)execute_64, (funcp)execute_118, (funcp)execute_119, (funcp)execute_120, (funcp)execute_121, (funcp)execute_122, (funcp)execute_123, (funcp)execute_124, (funcp)execute_126, (funcp)execute_127, (funcp)execute_128, (funcp)execute_129, (funcp)execute_133, (funcp)execute_137, (funcp)execute_138, (funcp)execute_139, (funcp)execute_140, (funcp)execute_141, (funcp)execute_142, (funcp)execute_145, (funcp)execute_147, (funcp)execute_148, (funcp)execute_149, (funcp)execute_150, (funcp)execute_151, (funcp)execute_152, (funcp)execute_153, (funcp)execute_154, (funcp)execute_155, (funcp)execute_156, (funcp)execute_157, (funcp)execute_158, (funcp)execute_159, (funcp)execute_160, (funcp)execute_50, (funcp)execute_51, (funcp)execute_52, (funcp)execute_53, (funcp)execute_130, (funcp)execute_131, (funcp)execute_132, (funcp)execute_55, (funcp)execute_56, (funcp)execute_57, (funcp)execute_58, (funcp)execute_134, (funcp)execute_135, (funcp)execute_136, (funcp)execute_60, (funcp)execute_61, (funcp)vlog_transfunc_eventcallback, (funcp)transaction_32, (funcp)vhdl_transfunc_eventcallback, (funcp)transaction_38, (funcp)transaction_39, (funcp)transaction_40, (funcp)transaction_49, (funcp)transaction_50, (funcp)transaction_51, (funcp)transaction_52};
+const int NumRelocateId= 102;
+
+void relocate(char *dp)
+{
+	iki_relocate(dp, "xsim.dir/QPSKTB_behav/xsim.reloc",  (void **)funcTab, 102);
+	iki_vhdl_file_variable_register(dp + 1320888);
+	iki_vhdl_file_variable_register(dp + 1320944);
+
+
+	/*Populate the transaction function pointer field in the whole net structure */
+}
+
+void sensitize(char *dp)
+{
+	iki_sensitize(dp, "xsim.dir/QPSKTB_behav/xsim.reloc");
+}
+
+	// Initialize Verilog nets in mixed simulation, for the cases when the value at time 0 should be propagated from the mixed language Vhdl net
+
+void wrapper_func_0(char *dp)
+
+{
+
+	iki_vlog_schedule_transaction_signal_fast_vhdl_value_time_0(dp + 1326232, dp + 1328928, 0, 0, 0, 0, 1, 1);
+
+	iki_vlog_schedule_transaction_signal_fast_vhdl_value_time_0(dp + 1327472, dp + 1328984, 0, 0, 0, 0, 1, 1);
+
+	iki_vlog_schedule_transaction_signal_fast_vhdl_value_time_0(dp + 1327432, dp + 1329040, 0, 16, 0, 16, 17, 1);
+
+}
+
+void simulate(char *dp)
+{
+		iki_schedule_processes_at_time_zero(dp, "xsim.dir/QPSKTB_behav/xsim.reloc");
+	wrapper_func_0(dp);
+
+	iki_execute_processes();
+
+	// Schedule resolution functions for the multiply driven Verilog nets that have strength
+	// Schedule transaction functions for the singly driven Verilog nets that have strength
+
+}
+#include "iki_bridge.h"
+void relocate(char *);
+
+void sensitize(char *);
+
+void simulate(char *);
+
+extern SYSTEMCLIB_IMP_DLLSPEC void local_register_implicit_channel(int, char*);
+extern void implicit_HDL_SCinstatiate();
+
+extern SYSTEMCLIB_IMP_DLLSPEC int xsim_argc_copy ;
+extern SYSTEMCLIB_IMP_DLLSPEC char** xsim_argv_copy ;
+
+int main(int argc, char **argv)
+{
+    iki_heap_initialize("ms", "isimmm", 0, 2147483648) ;
+    iki_set_sv_type_file_path_name("xsim.dir/QPSKTB_behav/xsim.svtype");
+    iki_set_crvs_dump_file_path_name("xsim.dir/QPSKTB_behav/xsim.crvsdump");
+    void* design_handle = iki_create_design("xsim.dir/QPSKTB_behav/xsim.mem", (void *)relocate, (void *)sensitize, (void *)simulate, 0, isimBridge_getWdbWriter(), 0, argc, argv);
+     iki_set_rc_trial_count(100);
+    (void) design_handle;
+    return iki_simulate_design();
+}
---
+++ QPSKBlock/QPSKBlock2.srcs/sources_1/new/QPSK.vhd
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 16.05.2022 16:47:24
+-- Design Name: 
+-- Module Name: QPSK - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity QPSK is
+    Port ( CLK : in STD_LOGIC;
+           TDATA_L : in STD_LOGIC_VECTOR (31 downto 0);
+           TVALID_L : in STD_LOGIC;
+           TVALID_R : OUT STD_LOGIC;
+           TREADY_L : out STD_LOGIC;
+           TREADY_R : in STD_LOGIC;
+           TDATA_R : out STD_LOGIC_VECTOR (23 downto 0));
+end QPSK;
+
+architecture Behavioral of QPSK is
+
+
+COMPONENT control
+    Port ( --axi_data_R : out STD_LOGIC_VECTOR (31 downto 0);
+           --axi_tvalid_R: out STD_LOGIC := '0';
+           --axi_tready_R: in STD_LOGIC;
+           axi_data : in STD_LOGIC_VECTOR (31 downto 0);
+           axi_tvalid: in STD_LOGIC;
+           ready: out STD_LOGIC := '0';
+           clk : in STD_LOGIC;
+           address : out std_logic_vector (16 downto 0) := (others => '0');
+           done : out std_logic := '0';
+           more : in std_logic
+           );
+end COMPONENT;
+
+
+COMPONENT blk_mem_gen_0
+  PORT (
+    clka : IN STD_LOGIC;
+    ena : IN STD_LOGIC;
+    addra : IN STD_LOGIC_VECTOR(16 DOWNTO 0);
+    douta : OUT STD_LOGIC_VECTOR(23 DOWNTO 0)
+  );
+END COMPONENT;
+
+signal addr : std_logic_vector(16 downto 0);
+
+
+constant enable : std_logic := '1';
+
+
+
+begin
+
+    Inst_control: control PORT MAP(
+        
+        clk => CLK,
+        axi_data => TDATA_L,
+        axi_tvalid => TVALID_L,
+        ready => TREADY_L,
+        done => TVALID_R,
+        more => TREADY_R,
+        
+        address => addr
+        );
+        
+     Inst_blk_mem_gen_0: blk_mem_gen_0 PORT MAP(
+     
+        clka => CLK,
+        ena => enable,
+        addra => addr,
+        douta => TDATA_R
+     
+     
+     );
+        
+
+
+end Behavioral;
